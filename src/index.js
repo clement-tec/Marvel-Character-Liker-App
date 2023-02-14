f4526d848d9926f43bfe38621550751d9290a236
clement senou <clementsenou74@gmail.com>
2023-02-14 16:00:08 -0600

    requirement done

--- src/index.js
+++ src/index.js
@@ -8,29 +8,38 @@ const favoriteList = document.querySelector(".favorite-list")
 const favoriteNames = document.querySelectorAll(".favorite-list li")
 const form = document.querySelector("#form")
 let currentCharacter
+let characters
+const dropDown = document.querySelector("#character-dropdown")
+
+
+dropDown.addEventListener("change", (e) => {
+    ul.innerHTML = ''
+    console.log(e.target.value)
+    const results = characters.filter(character => character.name[0] === e.target.value.toUpperCase())
+    results.forEach(renderCharacterList)
+})
 
 
 
 fetch('http://localhost:3000/characters')
 .then(Response => Response.json())
-.then(dataCharacters => dataCharacters.forEach(renderCharacterList))
+.then(dataCharacters => {
+    characters = dataCharacters
+    dataCharacters.forEach(renderCharacterList)}
+)
 
 const renderCharacterList = (character) => {
-  
     const li = document.createElement('li');
     li.style.cursor = 'pointer';
     li.textContent = character.name.toUpperCase();
     ul.appendChild(li);
-
+    
     li.addEventListener("click", () => {
         currentCharacter = character;
         characterName.textContent = character.name
         characterImage.src = character.image
         description.textContent = character.description
     })
-
-    
-    
 }
 
 addButton.addEventListener("click", () => {
@@ -63,7 +72,19 @@ form.addEventListener("submit", (e) => {
     }
 
     renderCharacterList(newCharacter)
+
+    fetch("http://localhost:3000/characters", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body : JSON.stringify(newCharacter)
+    })
 })
+
+
+
 
 
 //  helper function for the form
@@ -80,7 +101,7 @@ function fillIn(form, data) {
 
 
 fillIn(form, {
-    name: "Thanos",
-    image: "https://media.wired.com/photos/5ae242d3c302e41716cebb76/original/pass/InfinityWarComics_COVER.jpg",
-    description: "Thanos is a supervillain appearing in American comic books published by Marvel Comics. Created by writer-artist Jim Starlin, the character first appeared in The Invincible Iron Man #55. "
+    // name: "Thanos",
+    // image: "https://w0.peakpx.com/wallpaper/55/926/HD-wallpaper-thanos-crown-infinity-war-marvel-movie.jpg",
+    // description: "Thanos is a supervillain appearing in American comic books published by Marvel Comics. Created by writer-artist Jim Starlin, the character first appeared in The Invincible Iron Man #55. "
   })
